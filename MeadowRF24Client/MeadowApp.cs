1958784350c746475abafd59ccb6f533521311ea
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2020-05-18 16:18:05 +1200

    Improved display of NRF24 configuration and provided access to more options

--- MeadowRF24Client/MeadowApp.cs
+++ MeadowRF24Client/MeadowApp.cs
@@ -16,97 +16,103 @@
 //---------------------------------------------------------------------------------
 namespace devMobile.IoT.FieldGateway.nRF24Client
 {
-	using System;
-	using System.Text;
-	using System.Threading.Tasks;
+   using System;
+   using System.Text;
+   using System.Threading.Tasks;
 
-	using Meadow;
-	using Meadow.Devices;
-	using Meadow.Hardware;
+   using Meadow;
+   using Meadow.Devices;
+   using Meadow.Hardware;
 
-	using Radios.RF24;
+   using Radios.RF24;
 
-	public class MeadowApp : App<F7Micro, MeadowApp>
+   public class MeadowApp : App<F7Micro, MeadowApp>
    {
-		private const string BaseStationAddress = "Base1";
-		private const string DeviceAddress = "Dev01";
-		private const byte nRF24Channel = 15;
-		private RF24 Radio = new RF24();
+      private const string BaseStationAddress = "Base1";
+      private const string DeviceAddress = "Dev01";
+      private const byte nRF24Channel = 15;
+      private RF24 Radio = new RF24();
 
-		public MeadowApp()
+      public MeadowApp()
       {
-			try
-			{
-				var config = new Meadow.Hardware.SpiClockConfiguration(
-					2000,
-					SpiClockConfiguration.Mode.Mode0);
+         try
+         {
+            var config = new Meadow.Hardware.SpiClockConfiguration(
+                2000,
+                SpiClockConfiguration.Mode.Mode0);
 
-				ISpiBus spiBus = Device.CreateSpiBus(
-					Device.Pins.SCK,
-					Device.Pins.MOSI,
-					Device.Pins.MISO,config);
+            ISpiBus spiBus = Device.CreateSpiBus(
+                Device.Pins.SCK,
+                Device.Pins.MOSI,
+                Device.Pins.MISO,config);
 
-				Radio.OnDataReceived += Radio_OnDataReceived;
-				Radio.OnTransmitFailed += Radio_OnTransmitFailed;
-				Radio.OnTransmitSuccess += Radio_OnTransmitSuccess;
+            Radio.OnDataReceived += Radio_OnDataReceived;
+            Radio.OnTransmitFailed += Radio_OnTransmitFailed;
+            Radio.OnTransmitSuccess += Radio_OnTransmitSuccess;
 
-				Radio.Initialize(Device, spiBus, Device.Pins.D09, Device.Pins.D10, Device.Pins.D11);
-				Radio.Address = Encoding.UTF8.GetBytes(DeviceAddress);
+            Radio.Initialize(Device, spiBus, Device.Pins.D09, Device.Pins.D10, Device.Pins.D11);
+            Radio.Address = Encoding.UTF8.GetBytes(DeviceAddress);
 
-				Radio.Channel = nRF24Channel;
-				Radio.PowerLevel = PowerLevel.High;
-				Radio.DataRate = DataRate.DR250Kbps;
-				Radio.IsEnabled = true;
+            Radio.Channel = nRF24Channel;
+            //Radio.PowerLevel = PowerLevel.Max;
+            Radio.PowerLevel = PowerLevel.High;
+            //Radio.PowerLevel = PowerLevel.Low;
+            //Radio.PowerLevel = PowerLevel.Minimum;
+            Radio.DataRate = DataRate.DR250Kbps;
+            //Radio.DataRate = DataRate.DR1Mbps;
+            //Radio.DataRate = DataRate.DR2Mbps
+            Radio.IsEnabled = true;
+            Radio.IsAutoAcknowledge = true;
+            Radio.IsDyanmicAcknowledge = false;
+            Radio.IsDynamicPayload = true;
 
-				Radio.IsAutoAcknowledge = true;
-				Radio.IsDyanmicAcknowledge = false;
-				Radio.IsDynamicPayload = true;
+            Console.WriteLine($"Address: {Encoding.UTF8.GetString(Radio.Address)}");
+            Console.WriteLine($"Channel: {Radio.Channel}");
+            Console.WriteLine($"Frequency: {Radio.Frequency}");
+            Console.WriteLine($"PowerLevel: {Radio.PowerLevel}");
+            Console.WriteLine($"DataRate: {Radio.DataRate}");
+            Console.WriteLine($"IsEnabled: {Radio.IsEnabled}");
+            Console.WriteLine($"IsAutoAcknowledge: {Radio.IsAutoAcknowledge}");
+            Console.WriteLine($"IsDynamicAcknowledge: {Radio.IsDyanmicAcknowledge}");
+            Console.WriteLine($"IsDynamicPayload: {Radio.IsDynamicPayload}");
+            Console.WriteLine($"IsInitialized: {Radio.IsInitialized}");
+            Console.WriteLine($"IsPowered: {Radio.IsPowered}");
+         }
+         catch (Exception ex)
+         {
+            Console.WriteLine(ex.Message);
 
-				Console.WriteLine("Address: " + Encoding.UTF8.GetString(Radio.Address));
-				Console.WriteLine("PA: " + Radio.PowerLevel);
-				Console.WriteLine("IsAutoAcknowledge: " + Radio.IsAutoAcknowledge);
-				Console.WriteLine("Channel: " + Radio.Channel);
-				Console.WriteLine("DataRate: " + Radio.DataRate);
-				Console.WriteLine("IsDynamicAcknowledge: " + Radio.IsDyanmicAcknowledge);
-				Console.WriteLine("IsDynamicPayload: " + Radio.IsDynamicPayload);
-				Console.WriteLine("IsEnabled: " + Radio.IsEnabled);
-				Console.WriteLine("Frequency: " + Radio.Frequency);
-				Console.WriteLine("IsInitialized: " + Radio.IsInitialized);
-				Console.WriteLine("IsPowered: " + Radio.IsPowered);
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine(ex.Message);
-			}
+            return;
+         }
 
-			while (true)
-			{
-				string payload = "hello " + DateTime.Now.Second;
-				Console.WriteLine($"{DateTime.UtcNow:HH:mm:ss}-TX {payload.Length} byte message {payload}");
-				Radio.SendTo(Encoding.UTF8.GetBytes(BaseStationAddress), Encoding.UTF8.GetBytes(payload));
+         while (true)
+         {
+            string payload = "hello " + DateTime.Now.Second;
+            Console.WriteLine($"{DateTime.UtcNow:HH:mm:ss}-TX {payload.Length} byte message {payload}");
+            Radio.SendTo(Encoding.UTF8.GetBytes(BaseStationAddress), Encoding.UTF8.GetBytes(payload));
 
-				Task.Delay(30000).Wait();
-			}
-		}
+            Task.Delay(30000).Wait();
+         }
+      }
 
-		private void Radio_OnDataReceived(byte[] data)
-		{
-			// Display as Unicode
-			string unicodeText = Encoding.UTF8.GetString(data);
-			Console.WriteLine($"{DateTime.UtcNow:HH:mm:ss}-RX Unicode Length {0} Unicode Length {1} Unicode text {2}", data.Length, unicodeText.Length, unicodeText);
+      private void Radio_OnDataReceived(byte[] data)
+      {
+         // Display as Unicode
+         string unicodeText = Encoding.UTF8.GetString(data);
+         Console.WriteLine($"{DateTime.UtcNow:HH:mm:ss}-RX Data Length {data.Length} Unicode Length {unicodeText.Length} Unicode text {unicodeText}");
 
-			// display as hex
-			Console.WriteLine($"{DateTime.UtcNow:HH:mm:ss}-RX Hex Length {data.Length} Payload {BitConverter.ToString(data)}");
-		}
+         // display as hex
+         Console.WriteLine($"{DateTime.UtcNow:HH:mm:ss}-RX Hex Length {data.Length} Payload {BitConverter.ToString(data)}");
+      }
 
-		private void Radio_OnTransmitSuccess()
-		{
-			Console.WriteLine($"{DateTime.UtcNow:HH:mm:ss}-TX Succeeded!");
-		}
+      private void Radio_OnTransmitSuccess()
+      {
+         Console.WriteLine($"{DateTime.UtcNow:HH:mm:ss}-TX Succeeded!");
+      }
 
-		private void Radio_OnTransmitFailed()
-		{
-			Console.WriteLine($"{DateTime.UtcNow:HH:mm:ss}-TX failed!");
-		}
-	}
+      private void Radio_OnTransmitFailed()
+      {
+         Console.WriteLine($"{DateTime.UtcNow:HH:mm:ss}-TX failed!");
+      }
+   }
 }
